e868e185f7135e0ffe99bd058a84ed3b72ce31f5
Cedrick Garcia <icedgarcia@gmail.com>
2021-01-07 21:56:25 +0800

    Finalized :heavy_check_mark:

--- 6_article-preview-component-master/script.js
+++ 6_article-preview-component-master/script.js
@@ -1,8 +1,12 @@
-function displayPopup() {
-    let popup = document.getElementById('popup');
-    if (popup.style.display === 'flex') {
-        popup.style.display = 'none';
-    } else {
-        popup.style.display = 'flex';
-    }
-}
+// Trying arrow functions
+const showLinks = document.querySelector('.links');
+const btn = document.querySelector('.card__btn');
+
+btn.addEventListener('click', ()=> {
+    if (showLinks.style.display === 'none' || showLinks.style.display === "") {
+        showLinks.style.display = 'flex';
+      } else if (showLinks.style.display === 'flex') {
+        showLinks.style.display = 'none';
+      }
+})
+console.log("credits to ApplePieGirrafe")
