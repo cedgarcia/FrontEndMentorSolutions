94f7ccc969c6e7dc09edb9f65cec548fe0b92e86
Cedrick Garcia <icedgarcia@gmail.com>
2021-01-10 20:19:09 +0800

    Fin :heavy_check_mark:

---
+++ 8_coding-bootcamp-testimonials-slider-master/app.js
@@ -0,0 +1,27 @@
+// buttons
+const btnPrev = document.querySelector('#btn-prev');
+const btnNext = document.querySelector('#btn-next');
+const image = document.querySelector('.carousel__img'); //image
+
+const reviewText = document.querySelector('.review');
+const authorName = document.querySelector('.author__name');
+const authorTitle = document.querySelector('.author__job');
+
+let bit = true;
+
+function changeContent() {
+    if (bit) {
+        reviewText.textContent = '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”';
+        authorName.textContent = 'John Tarkpor';
+        authorTitle.textContent = 'Junior Frontend Developer';
+        image.src = 'app/images/image-john.jpg';
+    } else {
+        reviewText.textContent = '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”';
+        authorName.textContent = 'Tanya Sinclair';
+        authorTitle.textContent = 'UX Engineer';
+        image.src = 'app/images/image-tanya.jpg';
+    }
+    bit = !bit;
+}
+btnPrev.addEventListener('click', changeContent);
+btnNext.addEventListener('click', changeContent);
