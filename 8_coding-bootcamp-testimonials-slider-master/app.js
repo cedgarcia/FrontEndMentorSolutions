624a65ff37e8ce21e6f3301a03bc63a7fe1256e1
Cedrick Garcia <icedgarcia@gmail.com>
2021-01-10 20:30:59 +0800

    Change JS :heavy_check_mark:

--- 8_coding-bootcamp-testimonials-slider-master/app.js
+++ 8_coding-bootcamp-testimonials-slider-master/app.js
@@ -9,19 +9,34 @@ const authorTitle = document.querySelector('.author__job');
 
 let bit = true;
 
-function changeContent() {
+btnPrev.addEventListener('click', ()=> {
     if (bit) {
         reviewText.textContent = '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”';
         authorName.textContent = 'John Tarkpor';
         authorTitle.textContent = 'Junior Frontend Developer';
         image.src = 'app/images/image-john.jpg';
-    } else {
+        }
+    else {
         reviewText.textContent = '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”';
         authorName.textContent = 'Tanya Sinclair';
         authorTitle.textContent = 'UX Engineer';
         image.src = 'app/images/image-tanya.jpg';
     }
     bit = !bit;
-}
-btnPrev.addEventListener('click', changeContent);
-btnNext.addEventListener('click', changeContent);
+})
+btnNext.addEventListener('click', ()=> {
+    if (bit) {
+        reviewText.textContent = '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”';
+        authorName.textContent = 'John Tarkpor';
+        authorTitle.textContent = 'Junior Frontend Developer';
+        image.src = 'app/images/image-john.jpg';
+        }
+    else {
+        reviewText.textContent = '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”';
+        authorName.textContent = 'Tanya Sinclair';
+        authorTitle.textContent = 'UX Engineer';
+        image.src = 'app/images/image-tanya.jpg';
+    }
+    bit = !bit;
+})
+
