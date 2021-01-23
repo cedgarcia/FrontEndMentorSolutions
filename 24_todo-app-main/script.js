77855cb2dd7ff6d5f8654fded7eb8ad20cabee25
Cedrick Garcia <icedgarcia@gmail.com>
2021-01-23 21:36:19 +0800

    add

--- 24_todo-app-main/script.js
+++ 24_todo-app-main/script.js
@@ -8,39 +8,31 @@ document.querySelector('.changeTheme-btn').addEventListener('click', ()=> {
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 
-
-
 function addToDo(toDo){
-    
-   
-    const item = `<li class="item">
-            
+     const item = `<li class="item">
+                    <div class="circle"></div>
                     <p class="text">${toDo}</p>
+                    <div class= "close">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+                    </div>
            
                   </li>
-                `;
-    
+                `;   
     const position = "beforeend";
     
     list.insertAdjacentHTML(position, item);
 }
 
-// add an item to the list user the enter key
 document.addEventListener("keyup",function(even){
     if(event.keyCode == 13){
         const toDo = input.value;
-        
-        // if the input isn't empty
+
         if(toDo){
             addToDo(toDo);
             
-           
-        //     // add item to localstorage ( this code must be added where the LIST array is updated)
-        //     localStorage.setItem("TODO", JSON.stringify(LIST));
-            
-        //     id++;
+    
         }
-        // input.value = "";
+ 
     }
 });
 
