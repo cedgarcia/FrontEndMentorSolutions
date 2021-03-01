803148fd614f60e0280c5b8d2516123678f54e25
Cedrick Garcia <icedgarcia@gmail.com>
2021-03-01 22:00:28 +0800

    wip

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,17 @@ var nav = document.querySelector('.projects'); // document.getElementById( 'nav'
 var viewer = document.querySelector('.viewer');
 var iframe = document.querySelector('#iframe');
 
-var data = [{}];
+var data = [
+  {
+    id: 1,
+    name: 'Profile Card Component',
+    link:
+      'https://cedgarcia-front-end-mentor-solutions.vercel.app/1_profile-card-component-main/index.html',
+  },
+  {
+    id: 2,
+    name: 'Social Proof',
+    link:
+      'https://cedgarcia-front-end-mentor-solutions.vercel.app/2_social-proof-section-master/index.html',
+  },
+];
