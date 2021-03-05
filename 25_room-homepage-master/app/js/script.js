bf7ba64091869b75aab8e9f388c6aaefa86372d1
Cedrick Garcia <icedgarcia@gmail.com>
2021-03-05 10:59:54 +0800

    done Nav

--- 25_room-homepage-master/app/js/script.js
+++ 25_room-homepage-master/app/js/script.js
@@ -1,3 +1,8 @@
 const btn = document.querySelector('.burger-menu');
 const navList = document.querySelector('.nav__list');
 const nav = document.querySelector('.nav');
+
+btn.addEventListener('click', () => {
+  navList.classList.toggle('show');
+  btn.classList.toggle('burger-menu--active');
+});
