75dc03e17635eea91a6173cd33aa506503df0583
Cedrick Garcia <icedgarcia@gmail.com>
2021-03-05 13:06:33 +0800

    layout done

--- 25_room-homepage-master/app/js/script.js
+++ 25_room-homepage-master/app/js/script.js
@@ -1,8 +1,10 @@
 const btn = document.querySelector('.burger-menu');
 const navList = document.querySelector('.nav__list');
 const nav = document.querySelector('.nav');
+const body = document.querySelector('body');
 
 btn.addEventListener('click', () => {
+  body.classList.toggle('disable-scroll');
   navList.classList.toggle('show');
   btn.classList.toggle('burger-menu--active');
 });
