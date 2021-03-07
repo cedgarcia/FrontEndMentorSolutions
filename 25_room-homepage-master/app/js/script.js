85102930c15a3b9d7bc83bde450b0b51cdd0c9f2
Cedrick Garcia <icedgarcia@gmail.com>
2021-03-07 23:55:58 +0800

    Finalized:heavy_check_mark:

--- 25_room-homepage-master/app/js/script.js
+++ 25_room-homepage-master/app/js/script.js
@@ -2,11 +2,13 @@ const btn = document.querySelector('.burger-menu');
 const navList = document.querySelector('.nav__list');
 const nav = document.querySelector('.nav');
 const body = document.querySelector('body');
+const filter = document.querySelector('.filter');
 
 btn.addEventListener('click', () => {
   body.classList.toggle('disable-scroll');
   navList.classList.toggle('show');
-  btn.classList.toggle('burger-menu--active');
+  btn.classList.toggle('active');
+  filter.classList.toggle('active');
 });
 
 const SLIDETIME = 550; //ms
