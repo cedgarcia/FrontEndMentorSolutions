1e1c5a6426a602a7b2b2d73ed651f7d801f4ebf0
Cedrick Garcia <icedgarcia@gmail.com>
2021-03-10 17:24:31 +0800

    done add and delete todo

--- 24_todo-app-main/script.js
+++ 24_todo-app-main/script.js
@@ -7,7 +7,11 @@ let todoItems = [];
 
 function renderTodo(todo) {
   const item = document.querySelector(`[data-key='${todo.id}']`);
-
+  if (todo.deleted) {
+    // remove the item from the DOM
+    item.remove();
+    return;
+  }
   const isChecked = todo.checked ? 'active' : ''; // Mark/Unmark as completed
   const node = document.createElement('li');
   node.classList.add('item');
@@ -17,15 +21,13 @@ function renderTodo(todo) {
   node.innerHTML = `
   <label for="${todo.id}" class="circle completed"></label>
   <span class="text">${todo.text}</span>
-      <div class= "close" id="${todo.id}">
-         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
-     </div>
+
+         <svg class="close" id = "${todo.id}" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+
   `;
   if (item) {
-    // replace it
     list.replaceChild(node, item);
   } else {
-    // otherwise append it to the end of the list
     list.append(node);
   }
 }
@@ -53,20 +55,39 @@ form.addEventListener('submit', (event) => {
     input.focus();
   }
 });
+
+//
 const list = document.querySelector('#list');
 list.addEventListener('click', (event) => {
+  // mark as completed
   if (event.target.classList.contains('completed')) {
     const itemKey = event.target.parentElement.dataset.key;
     toggleDone(itemKey);
   }
+  // close when finished
+  if (event.target.classList.contains('close')) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
 });
+
 function toggleDone(key) {
-  // findIndex is an array method that returns the position of an element
-  // in the array.
   const index = todoItems.findIndex((item) => item.id === Number(key));
-  // Locate the todo item in the todoItems array and set its checked
-  // property to the opposite. That means, `true` will become `false` and vice
-  // versa.
+
   todoItems[index].checked = !todoItems[index].checked;
   renderTodo(todoItems[index]);
+}
+
+function deleteTodo(key) {
+  // find the corresponding todo object in the todoItems array
+  const index = todoItems.findIndex((item) => item.id === Number(key));
+  // Create a new object with properties of the current todo item
+  // and a `deleted` property which is set to true
+  const todo = {
+    deleted: true,
+    ...todoItems[index],
+  };
+  // remove the todo item from the array by filtering it out
+  todoItems = todoItems.filter((item) => item.id !== Number(key));
+  renderTodo(todo);
 }
